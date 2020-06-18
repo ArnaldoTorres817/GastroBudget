ef51644a5d07a2ca469d5ffb5f27bda2f72bc408
CarlosOriol <oriol.carlos3@gmail.com>
2020-06-18 19:18:34 -0400

    Search the correct data on the yelp api and added more cuisines

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,9 +4,9 @@ import AlgoliaPlaces from 'algolia-places-react';
 const axios = require('axios');
 
 const InputForm = () => {
-    const [ cuisine, setCuisine ] = useState('pizza');
-    const [ budget, setBudget ] = useState(1);
-    const [ location, setLocation ] = useState('United States');
+    const [cuisine, setCuisine] = useState('pizza');
+    const [budget, setBudget] = useState(1);
+    const [location, setLocation] = useState('United States');
 
     const searchOnYelpApi = e => {
         axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`, {
@@ -21,6 +21,7 @@ const InputForm = () => {
         })
             .then((res) => {
                 console.log(res)
+                console.log(res.data.businesses)
             })
             .catch((err) => {
                 console.log('error')
@@ -31,9 +32,38 @@ const InputForm = () => {
         <div id="input-form">
             <label htmlFor="cuisine">Cuisine</label>
             <select name="cuisine" id="cuisine" onChange={e => setCuisine(e.target.value)}>
-                <option value="pizza">Pizza</option>
-                <option value="chinese">Chinese</option>
-                <option value="italian">Italian</option>
+                <option value='acaibowls'>Acai Bowls</option>
+                <option value='bakeries'>Bakeries</option>
+                <option value='desserts'>Desserts</option>
+                <option value='foodtrucks'>Food Trucks</option>
+                <option value='icecream'>Ice Cream & Frozen Yogurt</option>
+                <option value='gourmet'>Specialty Food</option>
+                <option value='argentine'>Argentine</option>
+                <option value='brazilian'>Brazilian</option>
+                <option value='breakfast_brunch'>Breakfast & Brunch</option>
+                <option value='buffets'>Buffets</option>
+                <option value='burgers'> Burgers</option>
+                <option value='sandwiches'>Sandwiches</option>
+                <option value='cafes'>Cafes</option>
+                <option value='cafeteria'>Cafeteria</option>
+                <option value='caribbean'>Caribbean</option>
+                <option value='chinese'>Chinese</option>
+                <option value='cuban'>Cuban</option>
+                <option value='delis'>Delis</option>
+                <option value='diners'>Diners</option>
+                <option value='dinnertheater'>Dinner Theater</option>
+                <option value='french'>French</option>
+                <option value='greek'>Greek</option>
+                <option value='hawaiian'>Hawaiian</option>
+                <option value='indpak'>Indian</option>
+                <option value='italian'>Italian</option>
+                <option value='japanese'>Japanese</option>
+                <option value='latin'>Latin American</option>
+                <option value='mexican'>Mexican</option>
+                <option value='pizza'>Pizza</option>
+                <option value='seafood'>Seafood</option>
+                <option value='vegan'> Vegan</option>
+                <option value='vegetarian'>Vegetarian</option>
             </select>
             <label htmlFor="budget">Budget</label>
             <select name="budget" id="budget" onChange={e => setBudget(e.target.value)}>
@@ -55,8 +85,12 @@ const InputForm = () => {
                     type: 'city'
                 }}
 
-                onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>
-                    console.log(rawAnswer)}
+                 onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>
+                     setLocation(suggestion.name+","+suggestion.administrative)}
+
+                onSuggestions={({ rawAnswer, query, suggestions }) => 
+                     setLocation(query)}
+
             />
             <div className="flex-break"></div>
             <button type='button' id="search-button" onClick={searchOnYelpApi}>Search</button>
