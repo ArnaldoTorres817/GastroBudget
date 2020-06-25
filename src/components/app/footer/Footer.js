2bab414fede83764975f64c31a8b76b4c22b2f8c
Arnaldo <arnaldomtorres15@gmail.com>
2020-06-25 08:28:20 -0400

    refactor: applied css property order convention

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () =>
         <span className="footer-item">&copy; 2020 Arnaldo M. Torres Rodriguez, Carlos A. Oriol Rivera. All rights reserved.</span>
         <span className="footer-item"><a className="footer-link" href="https://github.com/Rogue817" target='_blank' rel="noopener noreferrer">Arnaldo's GitHub</a></span>
         <span className="footer-item"><a className="footer-link" href="https://github.com/CarlosOriol3" target='_blank' rel="noopener noreferrer">Carlos' GitHub</a></span>
-        <span className="footer-item">Restaurants information provided by&nbsp;&nbsp;<a href="https://www.yelp.com/" target="_blank" rel="noopener noreferrer"><img src={process.env.PUBLIC_URL + '/yelp_logo.png'} alt="Yelp" /></a></span>
+        <span className="footer-item">Restaurants information provided by&nbsp;&nbsp;<a href="https://www.yelp.com/" target="_blank" rel="noopener noreferrer"><img id='yelp-logo' src={process.env.PUBLIC_URL + '/yelp_logo.png'} alt="Yelp" /></a></span>
     </footer>
 
 export default Footer
