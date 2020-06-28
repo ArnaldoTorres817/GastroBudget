a881a266d07a6cb9ab0e85218746bb6e99c587f3
Arnaldo <arnaldomtorres15@gmail.com>
2020-06-28 10:56:39 -0400

    removed link from header :)

--- src/components/app/header/Header.js
+++ src/components/app/header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () =>
     <header>
-        <h1><a href="/" id="title">GastroBudget</a></h1>
+        <h1>GastroBudget</h1>
     </header>
 
 export default Header 
